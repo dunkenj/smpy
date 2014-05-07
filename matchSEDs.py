cbce84a12883dee615f30616eefa41f3484ce21d
dunkenj <ken@ngm21.plus.com>
2014-05-07 14:13:02 +0100

    Renamed files and removed unnecessary files

--- matchSEDs.py
+++ matchSEDs.py
@@ -5,6 +5,7 @@ import re
 import time
 import multiprocessing
 import atpy
+import logging
 
 from sm_functions import dist
 
@@ -79,8 +80,8 @@ Fitting function definition for later use by Processes
 
 """
 
-def galaxyFit(send_q, recv_q, printlock):
-    for gal in iter(send_q.get, 'STOP'):
+def galaxyFit(inputQueue, printQueue, printlock):
+    for gal in iter(inputQueue.get, 'STOP'):
         j = numpy.argmin(numpy.abs(z-zobs[gal])) # Find closest model redshift
 
 
@@ -164,8 +165,8 @@ def galaxyFit(send_q, recv_q, printlock):
             b = sum(numpy.isnan(numpy.ravel(chisq)))
             num_chosen = round(((a-b)/100.)*params.mode_mass_percentage)
 
-            chisq_order = numpy.argsort(chisq.flatten())
-            chisq_sorted = chisq.flatten()[chisq_order]
+            chisq_order = numpy.argsort(chisq)
+            chisq_sorted = chisq[chisq_order]
 
             Mass_sorted = scale.flatten()[chisq_order]
             Mass_sorted[num_chosen:] = numpy.nan
@@ -190,10 +191,143 @@ def galaxyFit(send_q, recv_q, printlock):
         else:
             print '{0:6d} {1:8f} {2:>5.2f} {3:>7.2f} {4:>8.1f} {5:>8.3f} {6:>5.1f} {7:>8.2f} {8:>3d} {9:>5.2f}'.format(gal+1,ID[gal],zobs[gal],Bestfit_Mass,chimin,tgs,tvs,taus,mis,numpy.log10(Bestfit_SFR))
 
-        output_string = '{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15} {16} {17}'.format(gal+1,ID[gal],zobs[gal],Bestfit_Mass,chimin,tgs,tvs,taus,mis, M_scaled[params.tot], MUV_scaled, Bestfit_Min, Bestfit_Max,minind,Bestfit_SFR,len(I),Bestfit_Beta,'\n')
+        if params.include_rest:
+            restframe_output = ' '.join(M_scaled.astype('str'))
+            output_string = '{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15} {16} {17}'.format(gal+1,ID[gal],zobs[gal],Bestfit_Mass,chimin,tgs,tvs,taus,mis, M_scaled[params.tot], MUV_scaled, minind,Bestfit_SFR,len(I),Bestfit_Beta,z[j],restframe_output,'\n')
+        else:
+            output_string = '{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15}'.format(gal+1,ID[gal],zobs[gal],Bestfit_Mass,chimin,tgs,tvs,taus,mis, M_scaled[params.tot], MUV_scaled, minind,Bestfit_SFR,len(I),Bestfit_Beta,'\n')
 
         printlock.release()
-        recv_q.put(output_string)
+        printQueue.put(output_string)
+
+def galaxyFitPlus(inputQueue, printQueue, printlock):
+    for gal in iter(inputQueue.get, 'STOP'):
+        mass_range = numpy.logspace(params.mass_min,params.mass_max,params.mass_bins)
+        muvBins = numpy.linspace(params.muv_max,params.muv_min,params.muv_bins)
+        betaBins = numpy.linspace(params.beta_min,params.beta_max,params.beta_bins)
+
+        j = numpy.argmin(numpy.abs(z-zobs[gal])) # Find closest model redshift
+
+        fo = obs[gal,:]
+        ferr = obs_err[gal,:]
+
+
+        fo[fo <= 0.] = 0.       # Set negative fluxes to zero
+        #print fo
+        I = (ferr > 0.)*(ferr < 1e6) # Find bands with no observation
+        fo = fo[I]                    # and exclude from fit
+        ferr = ferr[I]
+        fm = f[I,j,:]
+        #print fm[:,0,0,0,0]        
+
+        chisq = numpy.zeros((params.mass_bins,n_tg,n_tauv,n_tau,n_ssp))
+
+        for m, mass in enumerate(mass_range):
+            for i in range(len(fo)):
+                chisq[m] += (((mass*fm[i,:]-fo[i])**2)/(ferr[i])**2)
+
+        chimin,minind = numpy.nanmin(chisq), numpy.nanargmin(chisq)
+
+        likelihood = numpy.exp(-0.5*chisq)
+        likelihood /= likelihood.sum()
+        
+        if numpy.isinf(chimin) or numpy.isnan(minind) or len(fo) == 0:
+            output_string = '{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} \
+            {10} {11} {12} {13} {14} {15} {16} {17} {18}'.format(gal+1,ID[gal],zobs[gal],
+                                                                 -99,-99,-99,-99,-99,-99,
+                                                                 -99, -99, -99, -99,-99,-99,-99,
+                                                                 len(I),-99,'\n')
+            
+            massLikelihood = numpy.zeros(params.mass_bins+1)
+            massLikelihood[0] = gal
+            muvLikelihood = numpy.zeros(params.muv_bins+1)
+            muvLikelihood[0] = gal
+            betaLikelihood = numpy.zeros(params.beta_bins+1)
+            betaLikelihood[0] = gal
+            #tauLikelihood = numpy.zeros(n_tau)        
+            #tauLikelihood = numpy.insert(tauLikelihood,0,gal)        
+            printQueue.put([output_string,massLikelihood,muvLikelihood,betaLikelihood])
+            continue
+
+        #Find the coordinate of the model with the bestfit mass
+        si,tgi,tvi,ti,mi = numpy.unravel_index(minind,(params.mass_bins,n_tg,n_tauv,n_tau,n_ssp))
+        Bestfit_Mass = numpy.log10(mass_range[si]*params.flux_corr)
+        Bestfit_SFR = (mass_range[si]*SFR[tgi,ti,mi]*params.flux_corr)
+        Bestfit_Beta = beta[tgi,tvi,ti,mi]
+
+        F_rest = f[:,0]*mass_range[likelihood.argmax(0)]*params.flux_corr
+        restframeMags = 23.9 - 2.5*numpy.log10(F_rest)
+    
+        UV_rest = UV_flux[0]*mass_range[likelihood.argmax(0)]*params.flux_corr
+        restframeMUV = 23.9 - 2.5*numpy.log10(UV_rest)
+
+        Bestfit_restframeMags = restframeMags[:,tgi,tvi,ti,mi]
+        Bestfit_restframeMUV = restframeMUV[tgi,tvi,ti,mi]
+
+        if numpy.isnan(Bestfit_Mass) or numpy.isinf(chimin):
+            Bestfit_Mass = -99
+            #M_scaled[:] = -99
+            tgs = -99
+            tvs = -99
+            taus = -99
+            mis = -99
+
+        else:
+            tgs = tg[tgi]/1e9
+            tvs = tv[tvi]
+            taus = tau[ti]/1e9
+            mis = mi
+            
+        """
+        Likelihood array section:
+        """
+        
+        muvSize = numpy.diff(muvBins)[0]
+        betaSize = numpy.diff(betaBins)[0]
+        
+        massLikelihoods = likelihood.sum(axis=(1,2,3,4))            
+        massLikelihoods /= numpy.trapz(massLikelihoods,numpy.log10(mass_range))
+        massLikelihoods = numpy.insert(massLikelihoods,0,gal)
+            
+        muvLikelihoods = array.array('d')
+        for muv in muvBins:
+            window = ((restframeMUV.flatten() > muv - 0.5*muvSize) * 
+                        (restframeMUV.flatten() <= muv + 0.5*muvSize))
+            muvLikelihoods.append(numpy.sum(likelihood.sum(0).flatten()[window]))
+        
+        muvLikelihoods /= numpy.trapz(muvLikelihoods,muvBins)
+        muvLikelihoods = numpy.insert(muvLikelihoods,0,gal)
+        
+        betaLikelihoods = array.array('d')
+        for fbeta in betaBins:
+            window = ((beta.flatten() > fbeta - 0.5*betaSize)*
+                        (beta.flatten() <= fbeta + 0.5*betaSize))
+            betaLikelihoods.append(numpy.sum(likelihood.sum(0).flatten()[window]))
+            
+        betaLikelihoods /= numpy.trapz(betaLikelihoods,betaBins)        
+        betaLikelihoods = numpy.insert(betaLikelihoods,0,gal)
+        
+        """
+        #betaQueue.put(betaLikelihoods)
+        likelihood_shaped[numpy.invert(numpy.isfinite(likelihood_shaped))] = 0.
+        tauLikelihood = numpy.sum(likelihood_shaped,axis=(0,1,3))
+        tauLikelihood /= sum(tauLikelihood)
+        print tauLikelihood
+        tauLikelihood = numpy.insert(tauLikelihood,0,gal)
+        """
+        
+        printlock.acquire()
+
+        if calc_mode:
+            print '{0:4d} {1:6d} {2:>6.2f} {3:>8.1f} {4:>6.2f}'.format(gal+1,ID[gal],Bestfit_Mass,chimin, numpy.log10(Mode_Mass), '/n')
+        else:
+            print '{0:6d} {1:8f} {2:>5.2f} {3:>7.2f} {4:>8.1f} {5:>8.3f} {6:>5.1f} {7:>8.2f} {8:>3d} {9:>5.2f}'.format(gal+1,int(ID[gal]),zobs[gal],Bestfit_Mass,chimin,tgs,tvs,taus,mis,numpy.log10(Bestfit_SFR))
+
+        output_string = '{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15}'.format(gal+1,int(ID[gal]),zobs[gal],Bestfit_Mass,chimin,tgs,tvs,taus,mis,Bestfit_restframeMags[params.tot],Bestfit_restframeMUV,minind,Bestfit_SFR,len(I),Bestfit_Beta,'\n')
+
+        printlock.release()
+        printQueue.put([output_string, massLikelihoods, muvLikelihoods, betaLikelihoods])
+
 
 def getObservations(inputpath):
     input_data = atpy.Table(inputpath,type=params.input_format)
@@ -204,32 +338,38 @@ def getObservations(inputpath):
     zobs = input_data[params.z_col]
 
     filts_used = params.filts_used
+    filter_names = []
 
     k,l = 0,0
     for ii in range(len(column_names)):
-        if column_names[ii].lower().endswith(params.flux_col_end):
+        if column_names[ii].lower().endswith(params.flux_col_end.lower()):
             if k == 0:
                 fluxes = input_data[column_names[ii]]
             else:
                 fluxes = numpy.column_stack((fluxes,input_data[column_names[ii]]))
             k+=1
+            filter_names.append(column_names[ii])
 
-        if column_names[ii].lower().endswith(params.fluxerr_col_end):
+        if column_names[ii].lower().endswith(params.fluxerr_col_end.lower()):
             if l == 0:
                 fluxerrs = input_data[column_names[ii]]
             else:
                 fluxerrs = numpy.column_stack((fluxerrs,input_data[column_names[ii]]))
             l+=1
 
-    tot_mag = -2.5*numpy.log10(fluxes[:,params.tot]) + 23.9
-
     fluxes = fluxes[:,filts_used]
     fluxerrs = fluxerrs[:,filts_used]
 
-    return ID, zobs, tot_mag, fluxes, fluxerrs, k
+    return ID, zobs, fluxes, fluxerrs, k, filter_names
 
 
 if __name__ == '__main__':
+    
+    logfile = open("error.log", "w")
+    original_stderr = sys.stderr
+    sys.stderr = logfile
+    
+    start = time.time()
     """
     SECTION 1A
 
@@ -240,7 +380,7 @@ if __name__ == '__main__':
 
     """
 
-    ID, zobs, tot_mag, obs, obs_err, filters_found = getObservations(params.input_catalog)
+    ID, zobs, obs, obs_err, filters_found, filter_names = getObservations(params.input_catalog)
 
     """
     Section 1C
@@ -264,7 +404,7 @@ if __name__ == '__main__':
         n_tauv = Mshape[3]
         n_tau = Mshape[4]
         n_ssp = Mshape[5]
-
+        
         UV_flux = synmags['UV_flux']
         SFR = synmags['SFR']
 
@@ -287,9 +427,15 @@ if __name__ == '__main__':
     SECTION 1C
     Setting up output table
     """
-    if os.path.isfile("temp_output.txt"):
-        os.remove("temp_output.txt")
-    temp_file = open("temp_output.txt","w")
+    if os.path.isfile(params.output_name+"temp_output.txt"):
+        os.remove(params.output_name+"temp_output.txt")
+    temp_file = open(params.output_name+"temp_output.txt","w")
+    
+    mass_file = open(params.output_name+"temp_masses.prob", "wb")
+    muv_file = open(params.output_name+"temp_muv.prob", "wb")
+    beta_file = open(params.output_name+"temp_betas.prob", "wb")
+    tau_file = open(params.output_name+"temp_taus.prob","wb")
+    
 
     """
     SECTION 2
@@ -297,40 +443,72 @@ if __name__ == '__main__':
 
     """
     if calc_mode:
-        print '{0:>4s} {1:>6s} {2:>6s} {3:>8s} {4:>6s}'.format('N','ID','Best', 'chimin', 'Mode')
+        print '{0:>4s} {1:>8s} {2:>6s} {3:>8s} {4:>6s}'.format('N','ID','Best', 'chimin', 'Mode')
 
     else:
-        print '{0:>4s} {1:>6s} {2:>5s} {3:>6s} {4:>8s} {5:>10s} {6:>5s} {7:>12s} {8:>4s}'.format('N','ID','zobs','Best', 'chimin', 'tg', 'tauv','tau','SSP')
+        print '{0:>4s} {1:>8s} {2:>5s} {3:>6s} {4:>8s} {5:>10s} {6:>5s} {7:>12s} {8:>4s}'.format('N','ID','zobs','Best', 'chimin', 'tg', 'tauv','tau','SSP')
 
-    start = time.time()
+    loop_start = time.time()
     ncpus = numpy.clip(params.ncpus,1,multiprocessing.cpu_count())
 
-    send_q = multiprocessing.Queue()
-    recv_q = multiprocessing.Queue()
+    inputQueue = multiprocessing.Queue()
+    printQueue = multiprocessing.Queue()
+    #massQueue = multiprocessing.Queue()
+    #muvQueue = multiprocessing.Queue()
+    #betaQueue = multiprocessing.Queue()
+    
     printlock = multiprocessing.Lock()
+    
+    if params.calc_pdf:
+        fitFunction = galaxyFitPlus
+    else:
+        fitFunction = galaxyFit
+    
     for i in range( ncpus ):
-        multiprocessing.Process( target = galaxyFit, args = ( send_q , recv_q, printlock ) ).start()
-
+        #multiprocessing.Process( target = galaxyFit, args = ( inputQueue , printQueue, printlock ) ).start()
+        multiprocessing.Process( target = fitFunction,
+                                args = (inputQueue, printQueue, printlock ) ).start()
 
     # Put elements in the send queue for processing
     for gal in range( len(ID) ):
-        send_q.put( gal )
+        inputQueue.put( gal )
 
-    for gal in range( len(ID) ):
-        temp_file.write( recv_q.get() )
+    if params.calc_pdf:
+        for gal in range( len(ID) ):
+            printout, mass_array, muv_array, beta_array = printQueue.get()
+            temp_file.write( printout )
+            #print len(mass_array), len(muv_array), len(beta_array)
+            mass_array.tofile(mass_file)
+            muv_array.tofile(muv_file)
+            beta_array.tofile(beta_file)
+        #tau_array.tofile(tau_file)
+    else:
+        for gal in range( len(ID) ):
+            printout = printQueue.get()
+            temp_file.write( printout )
+            #print len(mass_array), len(muv_array), len(beta_array)
+       
 
     # Stop all the running processes
     for i in range( ncpus ):
-        send_q.put( 'STOP' )
+        inputQueue.put( 'STOP' )
 
     # Close both send and receive queues
-    send_q.close()
-    recv_q.close()
+    inputQueue.close()
+    printQueue.close()
+    #massQueue.close()
+    #muvQueue.close()
+    #betaQueue.close()
 
 
     temp_file.close()
+    mass_file.close()
+    muv_file.close()
+    beta_file.close()
+    #tau_file.close()
     #print results
-    print "Time taken: "+str(time.time()-start)
+    print "Fitting time taken: "+str(time.time()-loop_start)
+    print
 
 
     """
@@ -340,7 +518,7 @@ if __name__ == '__main__':
     while temp_file.closed == False:
         pause(0.1)
 
-    data = numpy.loadtxt("temp_output.txt")
+    data = numpy.loadtxt(params.output_name+"temp_output.txt")
     try:
         rows, cols = data.shape
     except:
@@ -348,9 +526,15 @@ if __name__ == '__main__':
 
     output = atpy.Table(name='Results')
 
-    names = ['N','ID','z','Bestfit_Mass','Bestfit_chi2','Age','Dust_Tau','SFH_Tau','SSP_Number','H_rest', 'M1500','Bestfit_Min', 'Bestfit_Max','temp_index','SFR','nfilts','Beta','MeanMass','MeanBeta','MeanM1500']
-    units = [None,None,None,'log(Ms)',None,'Gyr',None,'Gyr',None, 'AB_mags', 'AB_mags','log(Ms)','log(Ms)',None,'Ms/yr',None,None,'log(Ms)',None,'AB_mags']
-    types = ['i4','i4','f4','f4','f4','f4','f4','f4','i4', 'f4', 'f4','f4','f4','i4','f4','f4','i4','f4','f4','f4','f4']
+    names = ['N','ID','z','Bestfit_Mass','Bestfit_chi2','Age','Dust_Tau','SFH_Tau','SSP_Number','TotCol_rest', 'M1500','temp_index','SFR','nfilts','Beta','z_model']
+    units = [None,None,None,'log(Ms)',None,'Gyr',None,'Gyr',None, 'AB_mags', 'AB_mags',None,'Ms/yr',None,None,None]
+    types = ['i4','i4','f4','f4','f4','f4','f4','f4','i4', 'f4', 'f4','f4','f4','i4','f4','f4']
+    if params.include_rest:
+        for name in filter_names:
+            names.append(name[:-len(params.flux_col_end)]+'_rest')
+            units.append('AB_mags')
+            types.append('f4')
+        
     for col in range(cols):
         output.add_column(names[col], data[:,col], unit=units[col], dtype=types[col])
 
@@ -358,3 +542,102 @@ if __name__ == '__main__':
     if os.path.isfile(params.output_name):
         os.remove(params.output_name)
     output.write(params.output_name,type=params.table_format)
+    print('Catalog saved')
+    
+    if params.calc_pdf:
+        print
+        print('Opening, re-ordering and saving binaries:')
+    
+        print('{0:<20s}'.format('Masses')),
+        with open(mass_file.name) as mass_binary:
+            mass_likelihood = array.array('d')
+            mass_likelihood.fromfile(mass_binary,len(ID)*(params.mass_bins + 1))
+            mass_likelihood = numpy.reshape(mass_likelihood,(len(ID),(params.mass_bins + 1)))
+            mass_likelihood = mass_likelihood[numpy.argsort(mass_likelihood[:,0]),1:]
+    
+            output_binary = open(params.output_name+".masses.prob","wb")
+            mass_params = array.array('i',[len(ID),params.mass_bins])
+            mass_params.tofile(output_binary)
+        
+            mass_bins = numpy.linspace(params.mass_min,params.mass_max,params.mass_bins)
+            mass_bins.tofile(output_binary)
+        
+            for gal in range(len(ID)):
+                mass_likelihood[gal,:].tofile(output_binary)
+        
+            output_binary.close()
+            print('Done')
+    
+        print('{0:<20s}'.format('MUVs')),
+        with open(muv_file.name) as muv_binary:
+            muv_likelihood = array.array('d')
+            muv_likelihood.fromfile(muv_binary,len(ID)*(params.muv_bins + 1))
+            muv_likelihood = numpy.reshape(muv_likelihood,(len(ID),(params.muv_bins + 1)))
+            muv_likelihood = muv_likelihood[numpy.argsort(muv_likelihood[:,0]),1:]
+    
+            output_binary = open(params.output_name+".muv.prob","wb")
+            muv_params = array.array('i',[len(ID),params.muv_bins])
+            muv_params.tofile(output_binary)
+        
+            muv_bins = numpy.linspace(params.muv_max,params.muv_min,params.muv_bins)
+            muv_bins.tofile(output_binary)
+        
+            for gal in range(len(ID)):
+                muv_likelihood[gal,:].tofile(output_binary)
+        
+            output_binary.close()
+            print('Done')
+        
+        print('{0:<20s}'.format('Betas')),
+        with open(beta_file.name) as beta_binary:
+            beta_likelihood = array.array('d')
+            beta_likelihood.fromfile(beta_binary,len(ID)*(params.beta_bins + 1))
+            beta_likelihood = numpy.reshape(beta_likelihood,(len(ID),(params.beta_bins + 1)))
+            beta_likelihood = beta_likelihood[numpy.argsort(beta_likelihood[:,0]),1:]
+    
+            output_binary = open(params.output_name+".betas.prob","wb")
+            beta_params = array.array('i',[len(ID),params.beta_bins])
+            beta_params.tofile(output_binary)
+        
+            beta_bins = numpy.linspace(params.beta_min,params.beta_max,params.beta_bins)
+            beta_bins.tofile(output_binary)
+        
+            for gal in range(len(ID)):
+                beta_likelihood[gal,:].tofile(output_binary)
+        
+            output_binary.close()
+            print('Done')
+
+        """
+        print('{0:<20s}'.format('Taus')),
+        with open(tau_file.name) as tau_binary:
+            tau_likelihood = array.array('d')
+            tau_likelihood.fromfile(tau_binary,len(ID)*(n_tau + 1))
+            tau_likelihood = numpy.reshape(tau_likelihood,(len(ID),(n_tau + 1)))
+            tau_likelihood = tau_likelihood[numpy.argsort(tau_likelihood[:,0]),1:]
+    
+            output_binary = open(params.output_name+".sfh.prob","wb")
+            tau_params = array.array('i',[len(ID),len(params.tau)])
+            tau_params.tofile(output_binary)
+        
+            taus = numpy.array(params.tau)
+            taus.tofile(output_binary)
+        
+            for gal in range(len(ID)):
+                tau_likelihood[gal,:].tofile(output_binary)
+        
+            output_binary.close()
+            print('Done')
+    
+        """
+    os.remove(mass_file.name)
+    os.remove(muv_file.name)
+    os.remove(beta_file.name)
+    #os.remove(tau_file.name)
+
+    print
+    print "Total time taken: "+str(time.time()-start)
+    
+    sys.stderr = original_stderr
+    logfile.close()
+    
